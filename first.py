39308d26119b83329628c1bf6389c65f0c72e602
Sameera786 <ksameera7786@gmail.com>
2021-06-16 10:03:17 +0530

    first file added

---
+++ first.py
@@ -0,0 +1,9 @@
+#Exercise 1: Print these following lines
+#1. this is \\ double backslash
+#2. these are /\/\/\/\/\ mountains
+#3. \" \n \t \'
+
+
+print(r"\\")
+print("/\/\/\/\/\ ")
+print(r" \" \n \t \'")
