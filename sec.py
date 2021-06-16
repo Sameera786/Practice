a807a2fdee467e02b4bdc2f92f6d2271a0b2c08c
Sameera786 <ksameera7786@gmail.com>
2021-06-16 23:04:55 +0530

    Sec file is added

---
+++ sec.py
@@ -0,0 +1,9 @@
+#Exercide 2: Ask user to input n numbers and you have to print average of n numbers using string formatting.
+#(User should be able to insert n numbers in one line with comma seperated)
+
+print ("Enter no. with ',' as separator")
+List = []
+List = [int(i) for i in input().split(',')]
+print ("Sum is " + str(sum(List)))
+print ("length is " + str(len(List)))
+print ("Average = " + str(sum(List)/len(List)))
